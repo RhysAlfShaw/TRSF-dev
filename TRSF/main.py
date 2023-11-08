63558ff268c9076ab53fd07636c8054bcaa4ae41
Rhys Shaw <rhysc3p0@gmail.com>
2023-11-08 19:23:17 +0000

    simulation

--- TRSF/main.py
+++ TRSF/main.py
@@ -107,7 +107,7 @@ Detector of astRonomical soUrces in optIcal and raDio images
 
         else:
 
-            self.catalogue = compute_ph_components(self.image,self.local_bg,analysis_threshold_val=self.analysis_threshold,lifetime_limit=lifetime_limit,output=self.output)
+            self.catalogue = compute_ph_components(self.image,self.local_bg,analysis_threshold_val=self.analysis_threshold_val,lifetime_limit=lifetime_limit,output=self.output)
 
 
 
@@ -155,12 +155,15 @@ Detector of astRonomical soUrces in optIcal and raDio images
                 self.radio_characteristing()
                 
         if self.header:
-            Ra, Dec = self._xy_to_RaDec(self.catalogue['Xc'],self.catalogue['Yc'])
-            self.catalogue['RA'] = Ra
-            self.catalogue['DEC'] = Dec
-            self.catalogue['RA'] = self.catalogue['RA'].astype(float)
-            self.catalogue['DEC'] = self.catalogue['DEC'].astype(float)
-            
+            try:
+                Ra, Dec = self._xy_to_RaDec(self.catalogue['Xc'],self.catalogue['Yc'])
+                self.catalogue['RA'] = Ra
+                self.catalogue['DEC'] = Dec
+                self.catalogue['RA'] = self.catalogue['RA'].astype(float)
+                self.catalogue['DEC'] = self.catalogue['DEC'].astype(float)
+            except:
+                pass
+                
         self.set_types_of_dataframe()
 
     def set_types_of_dataframe(self):
@@ -187,8 +190,9 @@ Detector of astRonomical soUrces in optIcal and raDio images
         self.catalogue['Pa'] = self.catalogue['Pa'].astype(float)
         self.catalogue['parent_tag'] = self.catalogue['parent_tag'].astype(float)
         self.catalogue['Class'] = self.catalogue['Class'].astype(float)
-        self.catalogue['Y0_cutout'] = self.catalogue['Y0_cutout'].astype(float)
-        self.catalogue['X0_cutout'] = self.catalogue['X0_cutout'].astype(float)
+        if self.cutup:   
+            self.catalogue['Y0_cutout'] = self.catalogue['Y0_cutout'].astype(float)
+            self.catalogue['X0_cutout'] = self.catalogue['X0_cutout'].astype(float)
         self.catalogue['SNR'] = self.catalogue['SNR'].astype(float)
         self.catalogue['Noise'] = self.catalogue['Noise'].astype(float)
 
@@ -221,15 +225,20 @@ Detector of astRonomical soUrces in optIcal and raDio images
             self.Beam = self.calculate_beam()
             catalogue = self.catalogue
             image = self.image
-            pb_image = self.pb_image
+            if self.pb_PATH is not None:
+                pb_image = self.pb_image
+            else:
+                pb_image = None
             background_map = self.local_bg
 
 
         else:
             self.Beam = self.calculate_beam()
             image = cutout
-            pb_image = cutout_pb
+            if self.pb_PATH is not None:
+                pb_image = cutout_pb
             background_map = background_map
+
 
         # for each source in the catalogue create mask and measure properties. prephorm source flux correction.
 
@@ -238,8 +247,11 @@ Detector of astRonomical soUrces in optIcal and raDio images
         params = []
 
         for i, source in tqdm(catalogue.iterrows(),total=len(catalogue),desc='Calculating Source Properties..',disable=not self.output):
-            print(i)
-            mask = self.get_mask(row=source,image=image)
+            #print(i)
+            try:
+                mask = self.get_mask(row=source,image=image)
+            except:
+                continue
             #print(np.sum(mask))
             #plt.imshow(mask)
             #plt.show()
@@ -275,14 +287,14 @@ Detector of astRonomical soUrces in optIcal and raDio images
             else:
                 
                 background_mask = mask*background_map/self.sigma                 # fixed problem with slight offset.
-                
+                #print(self.Beam)
                 Flux_total = np.nansum(mask*image - background_mask)/self.Beam   # may need to be altered for universality.
                 Flux_peak = np.nanmax(mask*image) - background_mask[y_peak_loc,x_peak_loc]
             
             #pdb.set_trace() # for debugging
             background_mask = np.where(background_mask == 0, np.nan, background_mask) # set background mask to nan where there is no background.
             Noise = np.nanmean(background_mask)
-            print('Noise: ',Noise)
+            #print('Noise: ',Noise)
             Flux_total = Flux_total*Flux_correction_factor
             
             Area = np.sum(mask)
@@ -690,9 +702,9 @@ Detector of astRonomical soUrces in optIcal and raDio images
 
                 # Radio background is calculated using the median absolute deviation of the total image.
 
-                local_bg = self.radio_background(self.image)
-                local_bg = local_bg*self.sigma
-                analysis_threshold = local_bg*self.analysis_threshold
+                local_bg_o = self.radio_background(self.image)
+                local_bg = local_bg_o*self.sigma
+                analysis_threshold = local_bg_o*self.analysis_threshold
 
         if self.mode == 'Optical':
             # Optical background is calculated using a random sample of pixels
